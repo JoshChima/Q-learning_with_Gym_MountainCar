24ddd544fe8eb6be4945bf2cd07b7434b0cb6521
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-08-05 18:52:15 -0500

    Part 1

--- script1.py
+++ script1.py
@@ -1,13 +1,26 @@
+#https://pythonprogramming.net/q-learning-reinforcement-learning-python-tutorial/
 import gym
-
+import numpy as np
 env = gym.make("MountainCar-v0")
-
 env.reset() # always reset enviroment
 
-done = False
+print(env.observation_space.high)
+print(env.observation_space.low)
+print(env.action_space.n)
 
-while not done:
-    action = 2 # there are 3 actions push left/do nothing/ push right
-    new_state, reward, done, _ = env.step(action)
-    env.render() # this will show it
-env.close()
+#Split observation space into chuncks
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+print(discrete_os_win_size)
+
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+print(q_table.shape)
+print(q_table)
+# done = False
+
+# while not done:
+#     action = 2 # there are 3 actions push left/do nothing/ push right
+#     new_state, reward, done, _ = env.step(action)
+#     print(reward, new_state)
+#     env.render() # this will show it
+# env.close()
